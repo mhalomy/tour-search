7fadd5b84c5e7595516f27921e0bdc72d941907d
mhalomy <kryshflorante@gmail.com>
2018-07-12 14:54:26 +0200

    feat: allow search query and display resulting tour and map image accordingly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react';
 import Search from './components/Search';
+import Result from './components/Result';
+
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      term: ''
+      tours: '',
+      term: '',
+      result: [],
+      price: 0,
+      duration: 0,
+      filter: ''
     };
+  }
+
+  componentDidMount() {
+    fetch('https://api.myjson.com/bins/18x6yt')
+      .then(res => res.json())
+      .then(tours => this.setState({tours}));
+    // if(this.result.length) {
+    //   this.setState({result: this.result})
+    // }
+  }
+
+  // componentWillUpdate() {
+  //   this.performSearch();
+  // }
+
+  onTermChange(event) {
+    this.setState({term: event.target.value.capitalize()})
+  }
+
+  onKeyPress(event) {
+    if(event.key === 'Enter') {
+      this.performSearch();
+    }
+  }
+
+  performSearch() {
+    if(this.state.tours.length) {
+      let result = [];
+      this.state.tours.map(tour => {
+        if(tour.country.includes(this.state.term) || tour.destinations.includes(this.state.term)) {
+          result.push(tour)
+        }
+        return result;
+      });
+      this.setState({result})
+    }
   }
 
   render() {
@@ -15,7 +61,11 @@ class App extends Component {
         <Search
           value={this.state.term}
           placeholder="Where do you want to go?"
-          onChange={(event) => this.setState({term: event.target.value})} />
+          onChange={this.onTermChange.bind(this)}
+          onKeyPress={this.onKeyPress.bind(this)} />
+
+        <Result {...this.state} />
+
       </div>
     );
   }
